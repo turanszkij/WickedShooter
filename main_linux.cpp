7a70ba745a658408962aadcc5ea1fc7c169b06df
Turánszki János <turanszkij@users.noreply.github.com>
2025-02-03 09:35:52 +0100

    linux

---
+++ main_linux.cpp
@@ -0,0 +1,73 @@
+#include "stdafx.h"
+#include <SDL2/SDL.h>
+
+wi::Application application;
+
+bool is_startup = wi::helper::FileExists("startup.lua");
+
+int sdl_loop()
+{
+    SDL_Event event;
+
+    bool quit = false;
+    while (!quit)
+    {
+        SDL_PumpEvents();
+        application.Run();
+
+        if (!is_startup && wi::initializer::IsInitializeFinished())
+        {
+            wi::lua::RunFile(wi::helper::GetCurrentPath() + "/first_person.lua");
+            is_startup = true;
+        }
+
+        while( SDL_PollEvent(&event)) 
+        {
+            switch (event.type) 
+            {
+                case SDL_QUIT:      
+                    quit = true;
+                    break;
+                case SDL_WINDOWEVENT:
+                    switch (event.window.event) 
+                    {
+                    case SDL_WINDOWEVENT_CLOSE:
+                        quit = true;
+                        break;
+                    case SDL_WINDOWEVENT_RESIZED:
+                        application.SetWindow(application.window);
+                        break;
+                    default:
+                        break;
+                    }
+                default:
+                    break;
+            }
+        }
+    }
+
+    return 0;
+
+}
+
+int main(int argc, char *argv[])
+{
+    sdl2::sdlsystem_ptr_t system = sdl2::make_sdlsystem(SDL_INIT_EVERYTHING | SDL_INIT_EVENTS);
+    sdl2::window_ptr_t window = sdl2::make_window(
+            "Wicked Shooter",
+            SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+            1920, 1080,
+            SDL_WINDOW_SHOWN | SDL_WINDOW_VULKAN | SDL_WINDOW_ALLOW_HIGHDPI);
+
+    SDL_Event event;
+
+    application.SetWindow(window.get());
+
+    wi::backlog::setFontColor(wi::Color(130, 250, 180, 255));
+
+    int ret = sdl_loop();
+
+    SDL_Quit();
+
+    return ret;
+}
---
+++ main_windows.cpp
@@ -0,0 +1,219 @@
+// TemplateWindows.cpp : Defines the entry point for the application.
+//
+
+#include "stdafx.h"
+#include "main.h"
+
+#define MAX_LOADSTRING 100
+
+// Global Variables:
+HINSTANCE hInst;                                // current instance
+wi::Application application;					// Wicked Engine Application
+
+// Forward declarations of functions included in this code module:
+ATOM                MyRegisterClass(HINSTANCE hInstance);
+BOOL                InitInstance(HINSTANCE, int);
+LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+                     _In_opt_ HINSTANCE hPrevInstance,
+                     _In_ LPWSTR    lpCmdLine,
+                     _In_ int       nCmdShow)
+{
+    UNREFERENCED_PARAMETER(hPrevInstance);
+    UNREFERENCED_PARAMETER(lpCmdLine);
+
+    // TODO: Place code here.
+
+    BOOL dpi_success = SetProcessDpiAwarenessContext(DPI_AWARENESS_CONTEXT_PER_MONITOR_AWARE_V2);
+    assert(dpi_success);
+
+	wi::arguments::Parse(lpCmdLine); // if you wish to use command line arguments, here is a good place to parse them...
+
+    // Initialize global strings
+    MyRegisterClass(hInstance);
+
+    // Perform application initialization:
+    if (!InitInstance (hInstance, nCmdShow))
+    {
+        return FALSE;
+    }
+
+    HACCEL hAccelTable = LoadAccelerators(hInstance, nullptr);
+
+    wi::backlog::setFontColor(wi::Color(130, 250, 180, 255));
+
+    bool is_startup = wi::helper::FileExists("startup.lua");
+
+	MSG msg = { 0 };
+	while (msg.message != WM_QUIT)
+	{
+		if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE)) {
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+		else {
+
+			application.Run(); // run the update - render loop (mandatory)
+
+            if (!is_startup && wi::initializer::IsInitializeFinished())
+            {
+                wi::lua::RunFile(wi::helper::GetCurrentPath() + "/first_person.lua");
+                is_startup = true;
+            }
+		}
+	}
+
+    wi::jobsystem::ShutDown();
+
+    return (int) msg.wParam;
+}
+
+
+
+//
+//  FUNCTION: MyRegisterClass()
+//
+//  PURPOSE: Registers the window class.
+//
+ATOM MyRegisterClass(HINSTANCE hInstance)
+{
+    WNDCLASSEXW wcex;
+
+    wcex.cbSize = sizeof(WNDCLASSEX);
+
+    wcex.style          = CS_HREDRAW | CS_VREDRAW;
+    wcex.lpfnWndProc    = WndProc;
+    wcex.cbClsExtra     = 0;
+    wcex.cbWndExtra     = 0;
+    wcex.hInstance      = hInstance;
+    wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_TEMPLATEWINDOWS));
+    wcex.hCursor        = LoadCursor(nullptr, IDC_ARROW);
+    wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW+1);
+    wcex.lpszMenuName   = MAKEINTRESOURCEW(IDC_TEMPLATEWINDOWS);
+    wcex.lpszClassName  = L"Wicked Shooter";
+    wcex.hIconSm        = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+
+    return RegisterClassExW(&wcex);
+}
+
+//
+//   FUNCTION: InitInstance(HINSTANCE, int)
+//
+//   PURPOSE: Saves instance handle and creates main window
+//
+//   COMMENTS:
+//
+//        In this function, we save the instance handle in a global variable and
+//        create and display the main program window.
+//
+BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
+{
+   hInst = hInstance; // Store instance handle in our global variable
+
+   HWND hWnd = CreateWindowW(L"Wicked Shooter", L"Wicked Shooter", WS_OVERLAPPEDWINDOW,
+      CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
+
+   if (!hWnd)
+   {
+      return FALSE;
+   }
+
+   ShowWindow(hWnd, nCmdShow);
+   UpdateWindow(hWnd);
+
+
+   application.SetWindow(hWnd); // assign window handle (mandatory)
+
+
+   return TRUE;
+}
+
+//
+//  FUNCTION: WndProc(HWND, UINT, WPARAM, LPARAM)
+//
+//  PURPOSE:  Processes messages for the main window.
+//
+//  WM_COMMAND  - process the application menu
+//  WM_PAINT    - Paint the main window
+//  WM_DESTROY  - post a quit message and return
+//
+//
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    switch (message)
+    {
+    case WM_SETCURSOR:
+        break;
+    case WM_COMMAND:
+        {
+            return DefWindowProc(hWnd, message, wParam, lParam);
+        }
+        break;
+    case WM_SIZE:
+    case WM_DPICHANGED:
+		if (application.is_window_active)
+			application.SetWindow(hWnd);
+        break;
+	case WM_CHAR:
+		switch (wParam)
+		{
+		case VK_BACK:
+			wi::gui::TextInputField::DeleteFromInput();
+			break;
+		case VK_RETURN:
+			break;
+		default:
+		{
+			const wchar_t c = (const wchar_t)wParam;
+			wi::gui::TextInputField::AddInput(c);
+		}
+		break;
+		}
+		break;
+	case WM_INPUT:
+		wi::input::rawinput::ParseMessage((void*)lParam);
+		break;
+	case WM_KILLFOCUS:
+		application.is_window_active = false;
+		break;
+	case WM_SETFOCUS:
+		application.is_window_active = true;
+		break;
+    case WM_PAINT:
+        {
+            PAINTSTRUCT ps;
+            HDC hdc = BeginPaint(hWnd, &ps);
+            // TODO: Add any drawing code that uses hdc here...
+            EndPaint(hWnd, &ps);
+        }
+        break;
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        break;
+    default:
+        return DefWindowProc(hWnd, message, wParam, lParam);
+    }
+    return 0;
+}
+
+// Message handler for about box.
+INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    UNREFERENCED_PARAMETER(lParam);
+    switch (message)
+    {
+    case WM_INITDIALOG:
+        return (INT_PTR)TRUE;
+
+    case WM_COMMAND:
+        if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
+        {
+            EndDialog(hDlg, LOWORD(wParam));
+            return (INT_PTR)TRUE;
+        }
+        break;
+    }
+    return (INT_PTR)FALSE;
+}
