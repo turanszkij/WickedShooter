d6b460a0cb750215362c37c78400a64204f4cbc3
Turánszki János <turanszkij@users.noreply.github.com>
2025-02-06 08:08:54 +0100

    fixes

--- main_linux.cpp
+++ main_linux.cpp
@@ -4,21 +4,19 @@
 
 #include <SDL2/SDL.h>
 
-wi::Application application;
-
 int main(int argc, char *argv[])
 {
+    // Init SDL and create window:
     sdl2::sdlsystem_ptr_t system = sdl2::make_sdlsystem(SDL_INIT_EVERYTHING | SDL_INIT_EVENTS);
     sdl2::window_ptr_t window = sdl2::make_window(
-            "Wicked Shooter",
-            SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
-            1920, 1080,
-            SDL_WINDOW_SHOWN | SDL_WINDOW_VULKAN | SDL_WINDOW_ALLOW_HIGHDPI
+        "Wicked Shooter",
+        SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+        1920, 1080,
+        SDL_WINDOW_SHOWN | SDL_WINDOW_VULKAN | SDL_WINDOW_ALLOW_HIGHDPI
     );
 
 
-    // these masks are needed to tell SDL_CreateRGBSurface(From)
-    // to assume the data it gets is byte-wise RGB(A) data
+    // Set window icon:
     Uint32 rmask, gmask, bmask, amask;
 #if SDL_BYTEORDER == SDL_BIG_ENDIAN
     int shift = (gimp_image.bytes_per_pixel == 3) ? 8 : 0;
@@ -32,18 +30,20 @@ int main(int argc, char *argv[])
     bmask = 0x00ff0000;
     amask = (gimp_image.bytes_per_pixel == 3) ? 0 : 0xff000000;
 #endif
-    SDL_Surface* icon = SDL_CreateRGBSurfaceFrom((void*)gimp_image.pixel_data, gimp_image.width,
+    SDL_Surface* icon = SDL_CreateRGBSurfaceFrom(
+        (void*)gimp_image.pixel_data, gimp_image.width,
         gimp_image.height, gimp_image.bytes_per_pixel * 8, gimp_image.bytes_per_pixel * gimp_image.width,
-        rmask, gmask, bmask, amask);
+        rmask, gmask, bmask, amask
+    );
     SDL_SetWindowIcon(window.get(), icon);
     SDL_FreeSurface(icon);
 
 
-    SDL_Event event;
-
+    // Create and run Wicked Engine application:
+    wi::Application application;
     application.SetWindow(window.get());
 
-    wi::backlog::setFontColor(config::backlog_color);
+    wi::backlog::setFontColor(config::backlog_color); // customize backlog color for this app
 
     SDL_Event event;
 
@@ -62,10 +62,10 @@ int main(int argc, char *argv[])
 
         while (SDL_PollEvent(&event))
         {
+            bool textinput_action_delete = false;
+
             switch (event.type)
             {
-                bool textinput_action_delete = false;
-
             case SDL_QUIT:
                 quit = true;
                 break;
@@ -90,13 +90,15 @@ int main(int argc, char *argv[])
             case SDL_KEYDOWN:
                 if (event.key.keysym.scancode == SDL_SCANCODE_BACKSPACE
                     || event.key.keysym.scancode == SDL_SCANCODE_DELETE
-                    || event.key.keysym.scancode == SDL_SCANCODE_KP_BACKSPACE) {
+                    || event.key.keysym.scancode == SDL_SCANCODE_KP_BACKSPACE) 
+                {
                     wi::gui::TextInputField::DeleteFromInput();
                     textinput_action_delete = true;
                 }
                 break;
             case SDL_TEXTINPUT:
-                if (!textinput_action_delete) {
+                if (!textinput_action_delete) 
+                {
                     if (event.text.text[0] >= 21) {
                         wi::gui::TextInputField::AddInput(event.text.text[0]);
                     }
