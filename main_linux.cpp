543ca0daa9cf0fab90682d4c667d2c3c97cdc027
Turánszki János <turanszkij@users.noreply.github.com>
2025-02-06 07:54:07 +0100

    icon

--- main_linux.cpp
+++ main_linux.cpp
@@ -1,5 +1,6 @@
 #include "WickedEngine.h"
 #include "config.h"
+#include "icon.c"
 
 #include <SDL2/SDL.h>
 
@@ -12,7 +13,31 @@ int main(int argc, char *argv[])
             "Wicked Shooter",
             SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
             1920, 1080,
-            SDL_WINDOW_SHOWN | SDL_WINDOW_VULKAN | SDL_WINDOW_ALLOW_HIGHDPI);
+            SDL_WINDOW_SHOWN | SDL_WINDOW_VULKAN | SDL_WINDOW_ALLOW_HIGHDPI
+    );
+
+
+    // these masks are needed to tell SDL_CreateRGBSurface(From)
+    // to assume the data it gets is byte-wise RGB(A) data
+    Uint32 rmask, gmask, bmask, amask;
+#if SDL_BYTEORDER == SDL_BIG_ENDIAN
+    int shift = (gimp_image.bytes_per_pixel == 3) ? 8 : 0;
+    rmask = 0xff000000 >> shift;
+    gmask = 0x00ff0000 >> shift;
+    bmask = 0x0000ff00 >> shift;
+    amask = 0x000000ff >> shift;
+#else // little endian, like x86
+    rmask = 0x000000ff;
+    gmask = 0x0000ff00;
+    bmask = 0x00ff0000;
+    amask = (gimp_image.bytes_per_pixel == 3) ? 0 : 0xff000000;
+#endif
+    SDL_Surface* icon = SDL_CreateRGBSurfaceFrom((void*)gimp_image.pixel_data, gimp_image.width,
+        gimp_image.height, gimp_image.bytes_per_pixel * 8, gimp_image.bytes_per_pixel * gimp_image.width,
+        rmask, gmask, bmask, amask);
+    SDL_SetWindowIcon(window.get(), icon);
+    SDL_FreeSurface(icon);
+
 
     SDL_Event event;
 
